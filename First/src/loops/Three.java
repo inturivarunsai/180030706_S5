b5cde48fc7f4f29853702ac9bbb2e0483c87de96
intur <intur@LAPTOP-PUUAACIR>
2021-05-11 12:45:25 +0530

    first

---
+++ First/src/loops/Three.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.*;
+public class Three {
+
+
+ public static void main(String[] args) {
+  if (args.length ==0)
+        {
+        System.out.println("No Values");
+        }
+    else{
+        for(String i : args)
+            System.out.print(i+" , ");   
+        }
+ }
+
+}
