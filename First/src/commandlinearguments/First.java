917348dc9441598482deec35a607f860b35e042b
intur <intur@LAPTOP-PUUAACIR>
2021-05-15 09:30:01 +0530

    ....

---
+++ First/src/commandlinearguments/First.java
@@ -0,0 +1,11 @@
+package commandlinearguments;
+public class First {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(args[0]+" "+"Technologies"+" "+args[1]);
+
+	}
+
+}
